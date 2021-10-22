0b75f94f8c01e3829d486d9674159ad4df6f31d4
Minji Kim <minji1004y@gmail.com>
2021-10-22 21:37:09 +0900

    start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div>
+      <Main>
+        안녕
+      </Main>
+    </div>
+  );
+}
+
+export default App;
