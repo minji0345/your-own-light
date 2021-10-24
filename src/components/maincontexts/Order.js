f3d61a150d7ea5f6529d5386653e35b98c93638d
Minji Kim <minji1004y@gmail.com>
2021-10-24 16:52:40 +0900

    add next

--- src/components/maincontexts/Order.js
+++ src/components/maincontexts/Order.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './maincontexts.css';
 
 const cities = [];
@@ -22,6 +22,35 @@ function City() {
 };
 
 function Order() {
+
+    const moduleNum = 1;
+
+    const [moduleType, setModuleType] = useState(moduleNum)
+
+    function nextHandler(moduleNum){
+        moduleNum += 1;
+        return(
+            setModuleType(moduleNum)
+        )
+    }
+
+    function moduleHandler() {
+
+        if (moduleType === 1){
+            return(
+                <CityBlockTop />
+            );
+        } else if (moduleType === 2){
+            return(
+                <CityBlockMiddle/>
+            )
+        } else if (moduleType === 3){
+            return(
+                <CityBlockBase/>
+            )
+        }
+    }
+
     
     
     function CityBlockTop() {
@@ -36,16 +65,16 @@ function Order() {
 
     function CityBlockMiddle() {
         return(
-            <div class="city-list">
-                이미지 넣기
+            <div class="city-list" style={{overflowY:'scroll'}}>
+                middle
             </div>
         )
     }
 
     function CityBlockBase() {
         return(
-            <div class="city-list">
-                이미지 넣기
+            <div class="city-list" style={{overflowY:'scroll'}}>
+                base
             </div>
         )
     }
@@ -53,13 +82,28 @@ function Order() {
     return (
         <div className="main-order">
             <div className="city-display">
-                <div className="status-bar">1.top 2.middle 3.base</div>
-                <CityBlockTop/>
+                <div className="status-bar">
+                    <div className="status-list">
+                        <div className="status"> 1.top</div>
+                        <div className="status">2.middle</div>
+                        <div className="status"> 3.base</div>
+                    </div>
+                    <div className="next-button" onClick={() => nextHandler(moduleNum)}>
+                        next step
+                    </div>
+                    
+                </div>
+                <div>{moduleHandler()}</div>
             </div>
-            <div className="order-state"></div>
+            <div className="order-state">
+                <div className="order order-top"> Top </div>
+                <div className="order order-middle"> Middle </div>
+                <div className="order order-base"> Base </div>
+            </div>
         </div>
     );
 };
+
 
 
 export default Order;
